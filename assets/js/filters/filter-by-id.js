cf95ae569c526bb58e81911a606703afeedba041
Diego Montes <gomontes95@gmail.com>
2025-10-27 14:43:40 -0600

    14-create-a-filter-for-pattientshtml-that-find-the-patients-by-their-name-or-id • on develop 3

--- assets/js/filters/filter-by-id.js
+++ assets/js/filters/filter-by-id.js
@@ -2,11 +2,19 @@
  * Filter that search by name and/or id
  * @param { String } searchValue Comparative value
  * @param { [PropertyKey] } fields Property contents that could be compared, could be multiple.
- * @param { Object } data The Object to be filter
- * @returns { Object } New Object with only the filtered Property
+ * @param { Array } data The Array to be filter
+ * @returns { Array } New Array with only the filtered Property
  */
 const filterBy = (searchValue, fields = ['id', 'name'], data = []) => {
   if (!searchValue) return data;
+
+  if (!fieldSearchName) {
+    throw new Error('The parameter fieldSearchName should have a valid value');
+  }
+
+  if (!Array.isArray(data)) {
+    throw new TypeError('The parameter data should be an array');
+  }
 
   const search = String(searchValue).toLowerCase();
 
